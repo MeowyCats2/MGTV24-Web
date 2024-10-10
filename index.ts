875489ea58cfb888759b7577d745e67533b10945
. <.>
2024-10-09 21:39:11 -0700

    Pages and title

--- index.ts
+++ index.ts
@@ -164,7 +164,88 @@ const MessageSingleASTNode = async (node) => {
     }
   }
   
-const generatePage = (title, content, meta) => `<!DOCTYPE html>
+
+  const MessageASTNodesPlaintext = async (nodes) => {
+    if (Array.isArray(nodes)) {
+      const parsedNodes = []
+      for (const node of nodes) {
+        parsedNodes.push(await MessageSingleASTNodePlaintext(node))
+      }
+      return parsedNodes.join(" ")
+    } else {
+      return await MessageSingleASTNodePlaintext(nodes)
+    }
+}
+  
+const MessageSingleASTNodePlaintext = async (node) => {
+    if (!node) return null;
+  
+    const type = node.type;
+  
+    switch (type) {
+      case 'text':
+      case 'link':
+      case 'url':
+      case 'autolink':
+      case 'blockQuote':
+      case 'codeBlock':
+      case 'inlineCode':
+      case 'em':
+      case 'strong':
+      case 'underline':
+      case 'strikethrough':
+      case 'spoiler':
+        return node.content;
+
+      case 'br':
+      case 'newline':
+        return "\n"
+  
+      case 'channel': {
+        const id = node.id as string;
+        return "#" + (await client.channels.fetch(id))?.name;
+      }
+  
+      case 'role': {
+        const id = node.id as string;
+        return "@" + (await mgtvChannel.guild.roles.fetch(id))?.name;
+      }
+  
+      case 'user': {
+        const id = node.id as string;
+        const user = await client.users.fetch(id);
+        return "#" + (user.displayName ?? user.username);
+      }
+  
+      case 'here':
+      case 'everyone':
+        return "@" + node.type
+
+      case 'emoticon':
+        return typeof node.content === 'string' ? (
+          node.content
+        ) : (
+          await MessageASTNodesPlaintext(node.content)
+        );
+  
+      case 'emoji':
+      case 'twemoji':
+        return node.name;
+  
+      case 'timestamp':
+        return node.timestamp + " (" + node.format + ")"
+  
+      default: {
+        return type + ": " + (typeof node.content === 'string' ? (
+          node.content
+        ) : (
+          await MessageASTNodesPlaintext(node.content)
+        ));
+      }
+    }
+  }
+  
+const generatePage = (title: string, content: string, meta: string) => `<!DOCTYPE html>
 <html>
     <head>
         <title>${title} - MGTV24 News</title>
@@ -185,7 +266,7 @@ const generatePage = (title, content, meta) => `<!DOCTYPE html>
     </body>
 </html>`
 const parseHeadings = (content: String) => content.replaceAll(/(\n|^)\s*#\s(.+?)(<br \/>|$)/gs, "<h1>$2</h1>")
-const getHeading = (content: String) => content.match(/(\n|^)\s*#\s(.+?)(<br \/>|$)/s)?.[2]
+const getHeading = (content: String) => content.match(/^#\s(.+?)$/m)?.[1]
 const app = express()
 app.use("/static", express.static("static"))
 const generatePostList = async () => {
@@ -200,26 +281,28 @@ const generatePostList = async () => {
   }
   return parsedMessages
 }
+const parsedMessages = await generatePostList()
 const port = 3000
 app.get('/', async (req, res) => {
-  const parsedMessages = await generatePostList()
-  res.send(generatePage("News List", parsedMessages.join(""), `<meta property="og:title" content="News List">
+  res.send(generatePage("News List", parsedMessages.slice(+(req.query.page ?? 1) * 50 - 50, +(req.query.page ?? 1) * 50).join("") + `<br />${+req.query.page! > 1 ? `<a href="/?page=${+req.query.page! - 1}">Previous Page</a> ` : ""}<a href="/?page=${+(req.query.page ?? 1) + 1}">Next Page</a>`, `<meta property="og:title" content="News List">
 <meta property="og:description" content="Start reading MGTV24 news articles online today.">
 <meta property="og:site_name" content="MGTV24 Web &bull; ${parsedMessages.length} articles">`))
 })
 app.get('/search', async (req, res) => {
   if (!req.query.query) return res.redirect("/")
-  const parsedMessages = await generatePostList()
-  const results = parsedMessages.filter(data => data.replaceAll(/(<br \/>|^)\s*<i>.+?<\/i>/gs, "").toLowerCase().replaceAll(/\s/g, "").includes(req.query.query.toLowerCase().replaceAll(/\s/g, "")))
-  res.send(generatePage(`Search - ${req.query.query}`, `<span>${results.length} results</span>` + results.join(""), `<meta property="og:title" content="${req.query.query} - Search">
+  const results = parsedMessages.filter(data => data.replaceAll(/(<br \/>|^)\s*<i>.+?<\/i>/gs, "").toLowerCase().replaceAll(/\s/g, "").includes(req.query.query!.toLowerCase().replaceAll(/\s/g, "")))
+  res.send(generatePage(`Search - ${req.query.query}`, `<span>${results.length} results</span>` + results.slice(+(req.query.page ?? 1) * 50 - 50, +(req.query.page ?? 1) * 50).join("") + `<br />${+req.query.page! > 1 ? `<a href="/search?query=${req.query.query}&page=${+req.query.page! - 1}">Previous Page</a> ` : ""}<a href="/?query=${req.query.query}&page=${+(req.query.page ?? 1) + 1}">Next Page</a>`, `<meta property="og:title" content="${req.query.query} - Search">
   <meta property="og:description" content="Find out the search results for ${req.query.query} today here at MGTV24 Web.">
   <meta property="og:site_name" content="MGTV24 Web &bull; ${parsedMessages.length} articles">`))
 })
+const escapeHtml = (unsafe) => {
+  return unsafe.replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&#039;');
+}
 app.get('/post/:post', async (req, res) => {
   const message = await mgtvChannel.messages.fetch(req.params.post)
-  const parsedMessages = await generatePostList()
+  console.log(getHeading(message.content))
   const postData = await MessageASTNodes(parse(message.content, "extended"))
-  res.send(generatePage(getHeading(postData) ?? "Post", `<div><i>Written by <b>${message.author.displayName}</b> on <b>${message.createdAt.toLocaleString('en-US', { timeZone: "Europe/Berlin", dateStyle: "medium" })}</b></i><br />
+  res.send(generatePage(escapeHtml(await MessageASTNodesPlaintext(parse(getHeading(message.content) ?? "Post"))) ?? "Post", `<div><i>Written by <b>${message.author.displayName}</b> on <b>${message.createdAt.toLocaleString('en-US', { timeZone: "Europe/Berlin", dateStyle: "medium" })}</b></i><br />
     ${parseHeadings(postData)}
     ${message.attachments.size > 0 ? `
       <div class="attachmentList">
@@ -227,8 +310,8 @@ app.get('/post/:post', async (req, res) => {
       </div>
     ` : ""}</div>
   <h2>Other Recent Posts</h2>
-    ${parsedMessages.join("")}`, `<meta property="og:title" content="${getHeading(postData) ?? "Post"}">
-    <meta property="og:description" content="${postData}">
+    ${parsedMessages.slice(0, 50).join("")}<br/><a href="/?page=2">See more</a>`, `<meta property="og:title" content="${escapeHtml(await MessageASTNodesPlaintext(parse(getHeading(message.content) ?? "Post"))) ?? "Post"}">
+    <meta property="og:description" content="${escapeHtml(postData)}">
     <meta property="og:site_name" content="MGTV24 Web &bull; ${parsedMessages.length} articles">`))
 })
 app.listen(port, () => {
